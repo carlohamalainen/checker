775e0393702d10557195f5e7218169c02bf7b0d2
Carlo Hamalainen <carlo.hamalainen@gmail.com>
2013-03-25 12:39:46 +1000

    Show missing and orphaned checksums.

--- Checker.hs
+++ Checker.hs
@@ -79,6 +79,28 @@ checkStoredChecksum f = do
                              (Left  error)          -> liftIO $ putStrLn $ "error: " ++ error
         else liftIO $ putStrLn $ "checksum missing: " ++ f
 
+checkStoredChecksumIsMissing :: FilePath -> ReaderT FilePath IO ()
+checkStoredChecksumIsMissing f = do
+    topdir <- ask
+
+    md5file <- computeChecksumFilename f
+    hasChecksum <- liftM not $ isChecksumMissing f
+
+    if hasChecksum
+        then return ()
+        else liftIO $ putStrLn $ "checksum missing: " ++ f
+
+checkOrphanedChecksum :: FilePath -> ReaderT FilePath IO ()
+checkOrphanedChecksum md5file = do
+    topdir <- ask -- e.g. /tmp/foo
+
+    let originalFile = topdir </> (reverse $ drop (length ".md5sum") $ reverse $ joinPath $ drop (1 + (length $ splitPath topdir)) (splitPath md5file))
+
+    hasOriginalFile <- liftIO $ doesFileExist originalFile
+
+    if hasOriginalFile
+        then return ()
+        else liftIO $ putStrLn $ "orphaned checksum: " ++ md5file
 
 -- http://stackoverflow.com/questions/14259229/streaming-recursive-descent-of-a-directory-in-haskell/14261710#14261710
 getRecursiveContents :: (Proxy p) => FilePath -> () -> Producer p FilePath IO ()
@@ -188,17 +210,27 @@ reportFilesInS3ButNotLocal s3Prefix localPath = do
 go :: [String] -> IO ()
 go ["--checkall",        path]                              = runProxy $ getRecursiveContents path >-> useD (\file -> runReaderT (checkStoredChecksum file) path)
 go ["--computemissing",  path]                              = runProxy $ getRecursiveContents path >-> useD (\file -> runReaderT (computeChecksums    file) path)
+
+go ["--show-locals-missing-checksum",  path]                = runProxy $ getRecursiveContents path >-> useD (\file -> runReaderT (checkStoredChecksumIsMissing file) path)
+go ["--show-local-orphaned-checksums", path]                = runProxy $ getRecursiveContents (path </> ".md5sums") >-> useD (\file -> runReaderT (checkOrphanedChecksum file) path)
+
+
 go ["--update-s3-cache"]                                    = updateS3Cache
 go ["--fix-etags"]                                          = fixExtendedEtags
+
 go ["--check-checksums-local-vs-s3", s3Prefix, localPath]   = reportMismatchingMd5sumsLocalVsS3 s3Prefix localPath
 go ["--show-in-both", s3Prefix, localPath]                  = reportFilesInBoth s3Prefix localPath
 go ["--show-in-local-but-not-s3", s3Prefix, localPath]      = reportFilesInLocalButNotS3 s3Prefix localPath
 go ["--show-in-s3-but-not-local", s3Prefix, localPath]      = reportFilesInS3ButNotLocal s3Prefix localPath
+
 go _ = do
     putStrLn "Usage:"
     putStrLn ""
     putStrLn "    checker --checkall        <local dir>"
-    putStrLn "    checker --computemissing> <local dir>"
+    putStrLn "    checker --computemissing  <local dir>"
+    putStrLn ""
+    putStrLn "    checker --show-locals-missing-checksum   <local dir>"
+    putStrLn "    checker --show-local-orphaned-checksums  <local dir>"
     putStrLn ""
     putStrLn "    checker --update-s3-cache"
     putStrLn "    checker --fix-etags"
